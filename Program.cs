b58c50a7eaa168b015cfea002fbe1abae5f56cea
iradakhf <iradakhf@code.edu.az>
2022-07-02 17:21:18 +0400

    add file

---
+++ Program.cs
@@ -0,0 +1,212 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   // 50e qeder olan ededleri artan sira ile yazmaq
+            //byte number;
+            //byte i = 1;
+            //while (i <= 50)
+            //{
+            //    number = i;
+            //    Console.WriteLine(number);
+            //    i++;
+            //}
+
+            // 40a qeder 3e bolunen ededlerin sayini tapmaq
+            //byte count = 0;
+            //for (byte i = 1; i < 40; i++)
+            //{
+            //    if (i % 3 == 0)
+            //    {
+            //        count++;
+
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //}
+            //Console.WriteLine(count);
+            //100e qeder hem 3e hem 7e bolunen ededleri tapmaq ve gostermek
+
+            //for (int i = 1; i < 100; i++)
+            //{
+            //    if (i % 21 == 0)
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            //ededin reqemlerinin sayini tapmaq
+            //int number = 52063;
+            //int count = 0;
+            //for (int i = 0; i < number; i++)
+            //{
+            //    number = number / 10;
+
+            //    count++;
+            //}
+            //Console.WriteLine(count);
+
+            //ededi tersine yazmaq
+
+            //int remainder = 0;
+            //int reverseNumber = 0;
+            //int number = 920245;
+            //for (int i = 0; number>0; i++)
+            //{
+            //    remainder = number % 10;
+            //    reverseNumber = reverseNumber * 10 + remainder;
+            //    number /= 10;
+
+            //}
+            //Console.WriteLine(reverseNumber);
+
+
+
+            //}
+            // arraydeki en boyuk ededi tapmaq
+            //int[] number = { 45, 205, 36, 96 };
+            //int theBiggestNumber = number[0];
+
+            //for (int i = 1; i < number.Length; i++)
+            //{
+            //    if (number[i] > theBiggestNumber)
+            //    {
+            //        theBiggestNumber = number[i];
+            //    }
+            //}
+            //Console.WriteLine($"theBiggest is {+ theBiggestNumber}");
+
+            //int[] number = { 5, 2, 6, 0 };
+            //int theSmallestNumber = number[0];
+            //for (int i = 1; i < number.Length; i++)
+            //{
+            //    if (number[i] < theSmallestNumber)
+            //    {
+            //        theSmallestNumber = number[i];
+            //    }
+            //}
+            //Console.WriteLine(theSmallestNumber);
+
+            //int[] numbers = { 52, 63, 13, 69, 14 };
+            //Console.WriteLine(numbers.Length);
+
+            //int number =5;
+            //int divider = number / 2;
+            //bool isOdd = true;
+            //for (int i = 2; i <= divider; i++)
+            //{
+            //    if (number % i != 0)
+            //    {
+            //        isOdd = true;
+            //    }
+            //    else 
+            //    {
+            //        isOdd = false;
+            //        break;
+            //    }   
+
+            //}
+            //Console.WriteLine(isOdd);
+
+            // 0 ededinin nece defe islediyini tapmaq
+
+            //int[] number = {100, 0, 200, 400, 0, 500, 0};
+            //int count = 0;
+            //for (int i = 0; i < number.Length; i++)
+            //{
+            //    if (number[i] == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+            //Console.WriteLine(count);
+
+            //2 olculu 3*3e massivin butun elementlerini print etmek
+
+            //int[,] numbers = { { 5, 4, 2 }, { 5, 56, 26 }, { 2, 36, 45 } };
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        Console.Write(numbers[i, j]);
+            //        Console.Write("   ");
+            //    }
+            //    Console.WriteLine(" ");
+            //}
+
+
+            //int[,] numbers = { { 5, 4, 2 }, { 5, 56, 26 }, { 2, 36, 45 } };
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        if (i == 0) 
+            //        {
+            //            numbers[i, j] = 0;
+            //        }
+            //        Console.WriteLine(numbers[i, j]);
+            //    }
+
+            //}
+
+
+
+            //70den kicik olan ededlerden cut olanlari azalan sira ile gostermek
+
+
+            //for (int i = 70; i >= 2; i--)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //int[,] numbers = { { 5, 2, 3 }, { 1, 0, 23 }, { 78, 96, 7 } };
+            //for (int i = 0; i < numbers.GetLength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.GetLength(1); j++)
+            //    {
+            //        if (i == j)
+            //        {
+            //            numbers[i, j] = 0;
+            //        }
+            //        else
+            //        {
+            //            numbers[i, j] = 1;
+            //        }
+            //        Console.Write(numbers[i, j]);
+            //        Console.Write("   ");
+            //    }
+            //    Console.WriteLine("  ");
+            //}
+
+            //arraydeki menfi ededleri musbete cevirmek
+            //int[,] numbers = { { 4, 5, 0 }, { 54, -63, 78 }, { 100, -205, 32 } };
+            //for (int i = 0; i < numbers.getlength(0); i++)
+            //{
+            //    for (int j = 0; j < numbers.getlength(1); j++)
+            //    {
+            //        if (numbers[i, j] < 0)
+            //        {
+            //            numbers[i, j] *= (-1);
+            //        }
+
+            //        console.write(numbers[i, j]);
+            //        console.write(" ");
+            //    }
+            //    console.writeline("  ");
+            //}
+
+
+        }
+    }
+}
+
+
+    
+
